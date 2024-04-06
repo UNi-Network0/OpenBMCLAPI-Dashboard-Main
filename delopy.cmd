ca68633c236bcef9068480c4f516b0696911b485
un1fyz <3336016933@qq.com>
2024-04-06 11:43:04 +0800

    'deploy'

--- delopy.cmd
+++ delopy.cmd
@@ -1,4 +1,4 @@
-npm run docs:build && cd D:\UserData\Documents\vuepress\docs && git init && git add -A && git commit -m 'deploy' && git push -f git@github.com:UNi-Network0/OpenBMCLAPI-Dashboard-APIDOC.git  master:gh-pages
+npm run docs:build && cd D:\UserData\Documents\vuepress\docs && git init && git add -A && git commit -m 'deploy' && git push -f git@github.com:UNi-Network0/OpenBMCLAPI-Dashboard-APIDOC.git  master:main
 
 
 
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -14,7 +14,7 @@ export default hopeTheme({
 
   logo: "https://theme-hope-assets.vuejs.press/logo.svg",
 
-  repo: "/UNi-Network0/OpenBMCLAPI-Dashboard-APIDOC",
+  repo: "UNi-Network0/OpenBMCLAPI-Dashboard-APIDOC",
 
   docsDir: "src",
 
